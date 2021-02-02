8155abae09748cec5c70212c625ce29cd3a53f08
Orcioly Andrade Alves <orcioly@techmicro.com.br>
2021-02-02 10:10:56 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const ddd = 'asdfasd';
+
   return (
     <div className="App">
       <header className="App-header">
